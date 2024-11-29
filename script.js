3cd2be6a74312d5f319b446784efb760b819f65d
lhama <contato@lhama.design>
2024-11-29 20:07:46 -0300

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const secondHand = document.querySelector('.second-hand');
-const minuteHand = document.querySelectot('.minute-hand');
+const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
@@ -7,14 +7,14 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60)*6) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
     const hours = now.getHours();
-    const hoursDegrees = ((hour / 12) * 360) + ((minutes/60)*30) + 90;
+    const hoursDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
